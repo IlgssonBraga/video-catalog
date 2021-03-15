5e4a1fe97f81ebec02c67b9aec25e35eb9e276e8
Ilgsson Braga <ilgssonbraga@localhost.localdomain>
2021-03-14 21:25:09 -0300

    feat: http code 204 for delete method

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Put, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Param, Delete, HttpCode } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
@@ -29,6 +29,7 @@ export class CategoriesController {
   }
 
   @Delete(':id')
+  @HttpCode(204)
   remove(@Param('id') id: string) {
     return this.categoriesService.remove(+id);
   }
